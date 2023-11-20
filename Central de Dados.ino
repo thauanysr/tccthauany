69e59dbc73de47a823d744c0fc7683196641bf97
thauanysr <150561449+thauanysr@users.noreply.github.com>
2023-11-19 22:06:12 -0300

    Add files via upload
    Estação Remota = Realiza a aquisição dos dados dos sensores, realiza controle automático (parcial) do atuador e envia os dados para a Central de Dados. 
    Central de Dados = Trata os dados recebidos da Estação Remota e envia para o Servidor, envia dados de controle do atuador enviados pelo usuário através do Blynk para a estação remota.

---
+++ Central de Dados.ino
@@ -0,0 +1,108 @@
+
+#define BLYNK_TEMPLATE_ID "TMPL2l8guw1tS"                         //Define o identificador do template no Blynk.
+#define BLYNK_TEMPLATE_NAME "Automação de Sistema de Irrigação"   //Define o nome do template no Blynk.
+#define BLYNK_AUTH_TOKEN "50MPea4rS_-NllcAhRwUaOhJGt6kiNm6"       //Define o código de autenticação. 
+
+#define BLYNK_PRINT Serial                                       //Printa informações no port serial para fins de teste.
+#define ESP8266_BAUD 9600                                        //Define a velocidade do comunicação.
+
+#include <ESP8266_Lib.h>                                         //Inclusão da biblioteca do ESP8266.
+#include <BlynkSimpleShieldEsp8266.h>                            //Inclusão da biblioteca do Blynk.
+#include <SoftwareSerial.h>                                      //Inclusão da biblioteca que emula pinos TX e RX em outros pinos digitais.
+#include <RF24.h>                                                //Inclusão da biblioteca do módulo transceptor de radiofrequência.
+
+RF24 radio(9,10);                                                //Inicialização da biblioteca. 
+SoftwareSerial EspSerial(2, 3);                                  //Inicialização da biblioteca (RX, TX).                            
+
+char ssid[] = "iPhone de Thauany";                               //Define o nome da rede WiFi a ser conectada.
+char pass[] = "12345678";                                        //Define a senha da rede para conexão.
+
+BlynkTimer timer;                                                //Inicializa uma variável "timer" para sincronizar a conexão da rede. 
+
+struct DadosSup{                                                 //Pacote de dados que será recebido.
+  int valorUmi=0;
+  float valor_temp=0;
+  int valor_nivel=0;
+}dados_rec;
+
+float tempVal;                                                  //Definição da variável relativa ao sensor de temperatura.
+int umiVal;                                                     //Definição da variável relativa ao sensor de umidade do solo.
+int nivelVal;                                                   //Definição da variável relativa ao sensor de nível do reservatório.
+int releVal;                                                    //Definição da variável relativa ao estado do relé.
+int relePress;                                                  //Define a variável de estado anterior do relé.
+int Val;
+
+const uint64_t pipeOut = 0xE8E8F0F0E1LL;                        //Endereçamento do canal de comunicação.
+ESP8266 wifi(&EspSerial);                                       //Inicializa a conexão WiFi. 
+
+
+void setup() {
+  Serial.begin(9600);
+  radio.begin();                                               //Inicializa a comunicação sem fio.
+  radio.setPALevel( RF24_PA_LOW );                             //Define o nível do amplificador de potência.
+  radio.setDataRate( RF24_250KBPS );                           //Define a velocidade de dados a serem enviados (RF24_250KBPS / RF24_1MBPS  / RF24_2MBPS).
+  radio.openWritingPipe(pipeOut);                              //Define o endereço para envio de dados. 
+  radio.openReadingPipe(1, pipeOut);                           //Define o canal de recepção de dados.
+  radio.startListening();                                      //O módulo fica disponível para receber mensagem.
+  EspSerial.begin(ESP8266_BAUD);                               //Inicializa a comunicação com o módulo WiFi.
+  delay(10);
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, wifi, ssid, pass);             //Envia os dados de conexão para o Blynk.
+  timer.setInterval(1000L, sendUptime);                        //Define o tempo de envio de dados para o Blynk.
+
+  
+}
+
+void loop() {
+  for(int i=0; i<=49; i++){
+    if (radio.available()) {                                  //Verifica se há mensagem no canal. 
+      radio.read( &dados_rec, sizeof(DadosSup) );             //Se houver mensagem, esta é armazenada na variável "dados_rec".
+    }
+    delay(10); 
+  }
+  if(relePress != releVal){                                   //Verifica o estado atual do relé.
+    radio.stopListening();                                    //Interrompe a recepção de dados.
+    radio.write( &releVal , sizeof(releVal));                 //Envia o valor da variável "releVal" para a estação remota.
+    relePress = !relePress;                                   //Inverte o valor da variável de controle "relePress".
+    radio.startListening();                                   //O módulo reinicia a recepção de mensagem.
+  }
+
+  tempVal=dados_rec.valor_temp;                              //Lê o dado do pacote recebido e armazena da respectiva variável.       
+  umiVal=dados_rec.valorUmi;
+  nivelVal=dados_rec.valor_nivel;
+
+  Blynk.run();                                              //Inicializa o Blynk.
+  timer.run();                                              //Inicializa o timer do Blynk.
+
+  
+  Serial.print("Valor Temperatura:");                      //Printa valores para fins de teste.
+  Serial.println(dados_rec.valor_temp);
+  
+  Serial.print("Valor Umidade:");
+  Serial.print(dados_rec.valorUmi);
+  Serial.println("%");
+
+  Serial.print("Valor Nivel:");
+  Serial.println(dados_rec.valor_nivel);
+
+  Serial.print("Valor Rele:");
+  Serial.println(releVal);
+}
+
+void sendUptime(){                                        //Função que obtém a temperatura, umidade e nível e envia o valor para os datastreams.
+  Blynk.virtualWrite(V3, tempVal);                        //Envia ao datastream o valor de temperatura.
+  Blynk.virtualWrite(V2, nivelVal);                       //Envia ao datastream o valor do nível.
+  Blynk.virtualWrite(V0, umiVal);                         //Envia ao datastream o valor de umidade do solo.
+  
+}
+
+BLYNK_WRITE(V1){                                          //Obtém o valor do datastream.
+  int Val = param.asInt();                                //Armazena o valor do datastream na variável "Val". 
+  if( Val==1){                                            //Define a variável "releVal" de acordo com o valor obtido no datastream.
+    releVal=1;
+  }else{
+    releVal=0;
+  }
+  
+  Serial.print("Botao Pressionado");
+}
---
+++ Estação Remota.ino
@@ -0,0 +1,130 @@
+#include <OneWire.h>             //Inclusão de biblioteca do protocolo One-Wire do sensor de temperatura.
+#include <DallasTemperature.h>   //Inclusão de biblioteca do sensor de temperatura.
+#include <RF24.h>                //Incusão de biblioteca do transceptor de radiofrequência.
+
+
+
+#define leitura_temp 2          //Pino responsável pelo sensor de temperatura.
+#define leitura_umi A0          //Pino responsável pelo sensor de umidade do solo.
+#define leitura_nivel 3         //Pino responsável pelo sensor de nível do reservatório. 
+#define control_rele 4          //Pino responsável pelo controle do rele.
+
+int valorUmi;                   // Valor medido pelo sensor de umidade do solo (%). 
+float valor_temp;               // Valor medido pelo sensor de temperatura.
+int valor_nivel;                // Valor medido pelo sensor de nível do reservatório.
+int valor_analog_umi = 0;       // Valor medido pelo sensor de umidade do solo no pino analógico.
+int analogSoloSeco = 1022;      // Valor de referência para umidade mínima.
+int analogSoloMolhado = 500;    // Valor de referência para umidade máxima.
+int releVal;                    // Nível lógico do relé.
+
+struct DadosSup{                // Pacote de dados a serem enviados para a Central de Dados.
+  int valorUmi=0;
+  float valor_temp=0;
+  int valor_niv=0;
+}dados_env;
+
+const uint64_t pipeOut = 0xE8E8F0F0E1LL;         //Endereçamento do canal de comunicação. 
+
+OneWire oneWire(leitura_temp);                   //Inicialização da biblioteca OneWire.
+DallasTemperature temp_sensor(&oneWire);         //Inicialização da biblioteca DallasTemperature.
+RF24 radio(9, 10);                               //Inicialização da biblioteca RF24.     
+ 
+byte endereco[][6] = {"1node","2node"};       
+
+void setup() {                                   
+  Serial.begin(9600);
+
+  pinMode(leitura_nivel,INPUT);                 //Define o pino 3 como entrada.
+  pinMode(control_rele,OUTPUT);                 //Define o pino 4 como saída.
+   
+  temp_sensor.begin();                          //Inicializa o sensor de temperatura.
+  radio.begin();                                //Inicializa a comunicação sem fio.
+  radio.openWritingPipe(pipeOut);               //Define o endereço para envio de dados. 
+  radio.openReadingPipe(1,pipeOut);             //Define o canal de recepção de dados. 
+  radio.setPALevel(RF24_PA_LOW);                //Define o nível do amplificador de potência. 
+  radio.setDataRate( RF24_250KBPS );            //Define a velocidade de dados a serem enviados (RF24_250KBPS / RF24_1MBPS  / RF24_2MBPS). 
+  radio.startListening();                       //O módulo fica disponível para receber mensagem.
+  
+  digitalWrite(control_rele,LOW);               //Define o estado do relé.
+  
+}
+
+void loop() {                                   
+  for(int i=0; i<=99; i++){
+    if (radio.available()) {                   //Verifica se há mensagem no canal.                
+      radio.read( &releVal , sizeof(releVal)); //Se houver mensagem, esta é armazenada na variável "releVal".
+    }
+    delay(10); 
+  }
+  for( int i=1; i<=5; i++){
+    switch(i){
+      
+      case 1:                                  //Caso criado para fins de teste. 
+        Serial.println();                      //Sinaliza o ínicio da mensagem. 
+        Serial.print("Inicio");
+        Serial.println();
+        break;
+        
+      case 2:                                 //Responsável pela aquisição dos dados do sensor de temperatura.
+        Serial.println(); 
+        Serial.print("caso2");
+        Serial.println(); 
+        temp_sensor.requestTemperatures();    //Faz a requisão da leitura do sensor.
+        Serial.print("A temperatura é: ");    
+        dados_env.valor_temp = temp_sensor.getTempCByIndex(0);  //Armazena o valor de temperatura na variável do pacote de dados a ser enviado.
+        Serial.print(temp_sensor.getTempCByIndex(0));          
+        Serial.println();
+        break;
+        
+      case 3:                                //Responsável pela aquisição dos dados do sensor de umidade do solo.
+        Serial.println(); 
+        Serial.print("caso3");
+        Serial.println(); 
+
+        valor_analog_umi=analogRead(leitura_umi);                                 //Lê o pino analógico.
+        valorUmi = constrain(valor_analog_umi,analogSoloMolhado,analogSoloSeco);  //Restringe o valor lido no pino analógico ao intervalo composto pelos valores de referência.
+        valorUmi = map(valorUmi,analogSoloMolhado,analogSoloSeco,100,0);          //Realiza uma proporção dentro do intervalo de 0 a 100 com o valor obtido no intervalo de refência. 
+        Serial.print("Umidade do solo: "); 
+        Serial.print(valorUmi);                                                   
+        Serial.println("%"); 
+
+        dados_env.valorUmi=valorUmi;                   //Armazena o valor de umidade na variável do pacote de dados a ser enviado.  
+        
+        break;
+        
+      case 4:                                          //Responsável pela aquisição dos dados do sensor de nível do reservatório.
+        Serial.println(); 
+        Serial.print("caso4");
+        Serial.println(); 
+  
+        Serial.print("Valor Nível de Agua:");
+        valor_nivel = digitalRead(leitura_nivel);      //Realiza a leitura do pino 3.
+        Serial.println(valor_nivel);      
+        dados_env.valor_niv=valor_nivel;               //Armazena o valor do nível na variável do pacote de dados a ser enviado.  
+        Serial.println(); 
+        
+        
+        break;
+
+
+      case 5:                                         //Responsável pela aquisição dos dados do relé.
+        Serial.println(); 
+        Serial.print("caso5");
+        Serial.println(); 
+        Serial.print("Valore Rele:");
+        Serial.println(releVal);                      //Informa o valor da variável "releVal" recebido.
+        
+        break;
+      
+      default:
+        Serial.print("default");
+        Serial.println(); 
+      
+        
+    }       
+  }
+  radio.stopListening();                              //Interrompe a recepção de mensagem.
+  radio.write( &dados_env , sizeof(DadosSup));        //Envia o pacote de dados para a Central de Dados.
+  radio.startListening();                             //Módulo fica disponível para receber mensagem novamente.
+  
+}
